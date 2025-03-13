1ca2489eb62a6e2c16f224c40b7ed196f9f637a1
Md Abu Bokar <mdabu0212@gmail.com>
2025-03-14 00:50:18 +0600

    blink error fixed

--- Demo 1/blink.s
+++ Demo 1/blink.s
@@ -1,31 +1,33 @@
-.global main
-.equ RCC_APB2ENR, 0x40021018
-.equ GPIOC_BASE, 0x40011000
-.equ GPIOC_CRH, GPIOC_BASE + 0x04
-.equ GPIOC_ODR, GPIOC_BASE + 0x0C
-.equ LED_PIN, (1 << 13)
+    AREA RESET, CODE, READONLY
+    THUMB
 
-main:
-    LDR R0, =RCC_APB2ENR
-    LDR R1, [R0]
-    ORR R1, R1, #(1 << 4)   @ Enable GPIOC clock
+    EXPORT __main
+__main  
+    LDR R0, =0x40021018   ; RCC_APB2ENR address (Enable GPIOC Clock)
+    LDR R1, [R0]          
+    ORR R1, R1, #0x10     ; Set bit 4 (IOPCEN)
     STR R1, [R0]
 
-    LDR R0, =GPIOC_CRH
+    LDR R0, =0x40011004   ; GPIOC_CRH address (Configure PC13 as output)
     LDR R1, [R0]
-    BIC R1, R1, #(0xF << 20) @ Clear CNF & MODE for PC13
-    ORR R1, R1, #(0x1 << 20) @ Set PC13 as output (Max speed 10MHz)
+    BIC R1, R1, #(0xF << 20) ; Clear CNF & MODE for PC13
+    ORR R1, R1, #(0x1 << 20) ; MODE = 01 (Output, 10MHz)
     STR R1, [R0]
 
-blink:
-    LDR R0, =GPIOC_ODR
+blink_loop
+    LDR R0, =0x4001100C   ; GPIOC_ODR address
     LDR R1, [R0]
-    EOR R1, R1, #LED_PIN     @ Toggle PC13
+    EOR R1, R1, #(1 << 13) ; Toggle PC13
     STR R1, [R0]
 
-    MOV R2, #500000
-delay:
+    BL delay               ; Call delay function
+    B blink_loop           ; Repeat forever
+
+delay
+    LDR R2, =10000000         ; Adjust this for blinking speed
+delay_loop
     SUBS R2, R2, #1
-    BNE delay
+    BNE delay_loop
+    BX LR                   ; Return from delay
 
-    B blink
+    END
